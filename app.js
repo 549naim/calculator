8ebbec58d9cafb8c5d2fdba543baa051160510ca
naim <swenaim35549@gmail.como>
2022-01-28 22:02:17 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function buttonfunction(val){
+    form.display.value += val;
+}
+function equal(){
+    form.display.value=eval(form.display.value);
+}
+function clearScreen(){
+    form.display.value= '';
+}
